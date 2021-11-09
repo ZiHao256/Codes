19e8bfacbb090a4245742c4f76bc15506365b0f1
ZiHao256 <2638779206@qq.com>
2021-11-09 21:08:42 +0800

    view(admin, rstaff, rdelivery, employee)基本完成

--- TakeOutSystem/views.py
+++ TakeOutSystem/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.http import require_http_methods
 from django.core import serializers
 from django.http import JsonResponse
 import json
+from django.utils import timezone
 
 from .models import Employee, Balance_account, Location, Menu, Order, turnover, order_menu
 
@@ -267,6 +268,8 @@ def order_dish(request):
 
         order = Order(
             order_id=order_id,
+            order_status='订单开始',
+            build_time=timezone.localtime(),
             remark=request.GET.get('remark'),
             eat_in_store=request.GET.get('eat_in_store'),
             specify_delivery_time=request.GET.get('specify_delivery_time'),
@@ -296,3 +299,114 @@ def order_dish(request):
 
 @require_http_methods("GET")
 def pay(request):
+    response = {}
+    try:
+        order_id = request.GET.get('order_id')
+        order = Order.objects.get(order_id=order_id)
+
+        if request.GET.get('payment_metohd') is None:
+            # 支付方式为余额
+            balance = Balance_account.objects.get(account_id=order.payment_account_id)
+            balance -= order_menu.objects.get(order_id=order_id).amount
+            balance.save()
+
+            turn_id = request.GET.get('turn_id')
+            t = turnover(
+                turn_id=turn_id,
+                account_id=order.cus_id,
+                business_type='支付',
+                amount=order_menu.objects.get(order_id=order_id).amount
+            )
+            t.save()
+
+        order.payment_method=request.GET.get('payment_method')
+        order.payment_time=timezone.localtime()
+        order.order_status='完成支付'
+        order.save()
+
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+
+@require_http_methods("GET")
+def accept_dish_order(request):
+    response = {}
+    try:
+        order_id = request.GET.get('order_id')
+        order = Order.objects.get(order_id=order_id)
+        order_m = order_menu.objects.get(order_id = order_id)
+        dish = Menu.objects.get(dish_name=order_m.dish_name)
+        dish.stock -= 1
+        dish.save()
+
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+
+@require_http_methods("GET")
+def request_delivery(request):
+    response = {}
+    try:
+        order_id = request.GET.get('order_id')
+        order = Order.objects.get(order_id=order_id)
+        order.meal_complete_time=timezone.localtime()
+        order.order_status='完成备餐'
+
+        if order.eat_in_store == '堂食':
+            order.order_status='完成送达'
+
+        order.save()
+
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+
+@require_http_methods("GET")
+def accept_delivery_order(request):
+    response = {}
+    try:
+        order_id = request.GET.get('order_id')
+        order = Order.objects.get(order_id=order_id)
+        order.r_delivery_id = request.GET.get('r_delivery_id')
+        order.accept_order_time = timezone.localtime()
+        order.order_status='完成接单'
+        order.save()
+
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+
+@require_http_methods("GET")
+def delivered(request):
+    response = {}
+    try:
+        order_id = request.GET.get('order_id')
+        order = Order.objects.get(order_id=order_id)
+        order.delivery_time=timezone.localtime()
+        order.order_status='完成送达'
+        order.save()
+
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
+
+
