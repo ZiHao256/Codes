e091723433664c829d3be709c261558b8588a26b
ZiHao256 <2638779206@qq.com>
2021-11-12 15:53:18 +0800

    update url & models

--- TakeOutSystem/urls.py
+++ TakeOutSystem/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from .views import *
 
 urlpatterns = [
+    # administer
     url(r'add_one_employee$', add_one_employee, ),
     url(r'show_employee$', show_employee,),
     url(r'change_one_employee$', change_one_employee,),
@@ -11,8 +12,22 @@ urlpatterns = [
     url(r'add_one_location$', add_one_location),
     url(r'show_location$', show_location),
     url(r'change_one_location$', change_one_location),
+
+    # r_staff
     url(r'add_one_dish$', add_one_dish),
     url(r'show_dish$', show_dish),
     url(r'change_one_dish$', change_one_dish),
+    url(r'accept_dish_order$', accept_dish_order),
+    url(r'request_delivery$', request_delivery),
+
+    # Employee
+    url(r'order_dish$', order_dish),
+    url(r'pay$', pay),
+
+    # r_delivery
+    url(r'accept_delivery_order$', accept_delivery_order),
+    url(r'delivered$', delivered),
+
+
 
 ]
